583a9b0673394d55157a18d9976b5b0eb5c107e1
AkashThakur14 <ak9988385532@gmail.com>
2025-05-24 16:04:41 +0530

    contact form

--- script.js
+++ script.js
@@ -19,3 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault(); 
+  alert("Your message has been sent!");
+  this.reset(); 
+});
